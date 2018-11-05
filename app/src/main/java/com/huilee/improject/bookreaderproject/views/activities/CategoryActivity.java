0a08016d85ca89e5a3b9437a10e0540f6b7e7fba
huilee <huilee9999@gmail.com>
2018-11-04 19:48:58 -0500

    category activity

--- app/src/main/java/com/huilee/improject/bookreaderproject/views/activities/CategoryActivity.java
+++ app/src/main/java/com/huilee/improject/bookreaderproject/views/activities/CategoryActivity.java
@@ -7,12 +7,15 @@ import com.huilee.improject.bookreaderproject.R;
 
 /*
  * Created by huilee on 2018-11-01
+ * 分类页面
  */
-public class MainActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
